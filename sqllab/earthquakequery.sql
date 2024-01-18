7336a066a86579118f365be8aa12b05741f72399
Matthew Sigmond <matt@matthewsigmond.com>
2024-01-17 18:57:20 -0600

    Added custom SQL queries

--- sqllab/earthquakequery.sql
+++ sqllab/earthquakequery.sql
@@ -1,1 +1,8 @@
+-- Locate all earthquakes within a 10 degree box of Minneapolis.
 SELECT * FROM earthquakes WHERE latitude BETWEEN 40 AND 50 AND longitude BETWEEN -87 and -97 ORDER BY latitude DESC;
+
+-- Calculate the average magnitude of all earthquakes within the period
+SELECT AVG(mag) FROM earthquakes;
+
+-- Count the number of earthquakes in the period with a magnitude above 5
+SELECT COUNT(mag) FROM earthquakes WHERE magnitude > 5;
