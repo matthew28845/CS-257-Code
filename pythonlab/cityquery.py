2e983d22306f882947590d67fff156f5e325deae
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2024-01-24 09:24:28 -0600

    add some more queries

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -9,11 +9,16 @@ conn = psycopg2.connect(host="localhost",
 cur = conn.cursor()
 cur.execute("SELECT * FROM topcities WHERE city = 'Northfield';")
 row = cur.fetchone()
-if len(row) > 0:
+if row.type is not None:
     print("Northfield is in the top 1,000 cities. Its location is (", row[3], ") and its latitude is (", row[4], ")")
 else:
     print("Northfield is not in the top 1,000 cities. Very sad!")
-conn.commit()
+
+cur.execute("SELECT * FROM topcities ORDER BY population DESC")
+row = cur.fetchone()
+print("The biggest city in the US is ", row[0], "and its population is", row[2])
+
+cur.execute("SELECT * FROM topcities WHERE state = 'Minnesota' ORDER BY population ASC")
 
 cur.close()
 conn.close()
