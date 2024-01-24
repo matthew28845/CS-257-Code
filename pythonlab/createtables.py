5681417e343e641970934df8a901ef293a631d5c
Matthew Sigmond <43422956+matthew28845@users.noreply.github.com>
2024-01-24 16:48:39 -0600

    one more try 4 entering city and add code to create abbreviations table

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -9,6 +9,8 @@ conn = psycopg2.connect(host="localhost",
 cur = conn.cursor()
 cur.execute("DROP TABLE IF EXISTS topcities;")
 cur.execute("CREATE TABLE topcities (city TEXT, state TEXT, population INTEGER, latitude DECIMAL, longitude DECIMAL);")
+cur.execute("DROP TABLE IF EXISTS stateabbrs")
+cur.execute("CREATE TABLE stateabbrs (state TEXT, abbr TEXT);")
 conn.commit()
 
 cur.close()
