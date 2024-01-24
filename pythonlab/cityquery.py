983cc6a5b804068b99bdb8f8f143f971fa90dd23
Matthew Sigmond <43422956+matthew28845@users.noreply.github.com>
2024-01-24 16:33:09 -0600

    add comments and fix stupid mistakes

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+# Connect to the SQL server hosted on the local machine
 conn = psycopg2.connect(host="localhost",
                         port=5432,
                         database="sigmondm",
@@ -7,6 +8,8 @@ conn = psycopg2.connect(host="localhost",
                         password="pies347cash")
 
 cur = conn.cursor()
+
+# See if Northfield is in the list, and if so tell its coordinates
 cur.execute("SELECT * FROM topcities WHERE city = 'Northfield';")
 row = cur.fetchone()
 if row is not None:
@@ -14,32 +17,39 @@ if row is not None:
 else:
     print("Northfield is not in the top 1,000 cities. Very sad!")
 
+# List the biggest city in the US
 cur.execute("SELECT * FROM topcities ORDER BY population DESC")
 row = cur.fetchone()
 print("The biggest city in the US is ", row[0],"and its population is", row[2])
 
+#List the smallest city in Minnesota within the top 1000
 cur.execute("SELECT * FROM topcities WHERE state = 'Minnesota' ORDER BY population ASC")
 row = cur.fetchone()
 print("The smallest city in Minnesota within the top 1000 is", row[0], "and its population is", row[2])
 
-cur.execute("SELECT * FROM topcities ORDER BY longitude DESC")
+#List the westernmost city in the top 1000
+cur.execute("SELECT * FROM topcities ORDER BY longitude ASC")
 row = cur.fetchone()
 print("The farthest west US city in the top 1000 is", row[0], "and its population is", row[2])
 
-cur.execute("SELECT * FROM topcities ORDER BY longitude ASC")
+#List the easternmost city in the top 1000
+cur.execute("SELECT * FROM topcities ORDER BY longitude DESC")
 row = cur.fetchone()
 print("The farthest east US city in the top 1000 is", row[0], "and its population is", row[2])
 
+#List the northernmost city in the top 1000
 cur.execute("SELECT * FROM topcities ORDER BY latitude DESC")
 row = cur.fetchone()
 print("The farthest north US city in the top 1000 is", row[0], "and its population is", row[2])
 
+#List the southernmost city in the top 1000
 cur.execute("SELECT * FROM topcities ORDER BY latitude ASC")
 row = cur.fetchone()
 print("The farthest south US city in the top 1000 is", row[0], "and its population is", row[2])
 
+# Let the user input a state, and then print out the sum of the population of the top cities in that state.
 myState = input()
-sql = "SELECT SUM(population) FROM topcities WHERE state ="
+sql = "SELECT SUM(population) FROM topcities WHERE state = %s"
 cur.execute(sql, myState)
 
 cur.close()
