e24d638117b5a9ceb0aa0c7925065bab9ee1ec4e
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2024-01-24 09:31:38 -0600

    change some queries

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -9,7 +9,7 @@ conn = psycopg2.connect(host="localhost",
 cur = conn.cursor()
 cur.execute("SELECT * FROM topcities WHERE city = 'Northfield';")
 row = cur.fetchone()
-if row.type is not None:
+if row is not None:
     print("Northfield is in the top 1,000 cities. Its location is (", row[3], ") and its latitude is (", row[4], ")")
 else:
     print("Northfield is not in the top 1,000 cities. Very sad!")
@@ -19,6 +19,13 @@ row = cur.fetchone()
 print("The biggest city in the US is ", row[0], "and its population is", row[2])
 
 cur.execute("SELECT * FROM topcities WHERE state = 'Minnesota' ORDER BY population ASC")
+row = cur.fetchone()
+
+print("The smallest city in Minnesota within the top 1000 is ", row[0], "and its population is", row[2])
+
+cur.execute("SELECT * FROM topcities ORDER BY longitude DESC")
+row = cur.fetchone()
+print("The farthest east US city in the top 1000 is ", row[0], "and its population is", row[2])
 
 cur.close()
 conn.close()
