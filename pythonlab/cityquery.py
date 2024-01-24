e19b336982dca546d5c946d37e5c89a577f67512
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2024-01-24 09:40:03 -0600

    almost finished with queries

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -16,16 +16,31 @@ else:
 
 cur.execute("SELECT * FROM topcities ORDER BY population DESC")
 row = cur.fetchone()
-print("The biggest city in the US is ", row[0], "and its population is", row[2])
+print("The biggest city in the US is ", row[0],"and its population is", row[2])
 
 cur.execute("SELECT * FROM topcities WHERE state = 'Minnesota' ORDER BY population ASC")
 row = cur.fetchone()
-
-print("The smallest city in Minnesota within the top 1000 is ", row[0], "and its population is", row[2])
+print("The smallest city in Minnesota within the top 1000 is", row[0], "and its population is", row[2])
 
 cur.execute("SELECT * FROM topcities ORDER BY longitude DESC")
 row = cur.fetchone()
-print("The farthest east US city in the top 1000 is ", row[0], "and its population is", row[2])
+print("The farthest west US city in the top 1000 is", row[0], "and its population is", row[2])
+
+cur.execute("SELECT * FROM topcities ORDER BY longitude ASC")
+row = cur.fetchone()
+print("The farthest east US city in the top 1000 is", row[0], "and its population is", row[2])
+
+cur.execute("SELECT * FROM topcities ORDER BY latitude DESC")
+row = cur.fetchone()
+print("The farthest north US city in the top 1000 is", row[0], "and its population is", row[2])
+
+cur.execute("SELECT * FROM topcities ORDER BY latitude ASC")
+row = cur.fetchone()
+print("The farthest south US city in the top 1000 is", row[0], "and its population is", row[2])
+
+myState = input()
+sql = "SELECT SUM(population) FROM topcities WHERE state ="
+cur.execute(sql, myState)
 
 cur.close()
 conn.close()
