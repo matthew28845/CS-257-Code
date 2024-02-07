5cb389f495438c0c63e8278984e08c9dfa3e7192
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2024-02-07 09:03:48 -0600

    first test of population thingy

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -1,7 +1,14 @@
 import flask
+import psycopg2
 
 app = flask.Flask(__name__)
+conn = psycopg2.connect(host="localhost",
+                        port=5432,
+                        database="sigmondm",
+                        user="sigmondm",
+                        password="pies347cash")
 
+cur = conn.cursor()
 #
 @app.route('/hello')
 def my_function():
@@ -11,7 +18,9 @@ def my_function():
 def my_display(word1, word2):
     the_string = "The words are: " + word1 + " and " + word2;
     return the_string
-
+@app.route('/color/<word1>')
+def my_color(word1):
+    return '<h1 style="color:Red">' + word1 + '</h1>'
 @app.route('/add/<word1>/<word2>')
 def my_addition(word1, word2):
     word1 = int(word1)
@@ -19,11 +28,17 @@ def my_addition(word1, word2):
     the_sum = word1 + word2
     the_sum = str(the_sum)
     the_string = "The sum is: " + the_sum;
-    return the_sum
+    return the_string
 
-@app.route('/color/<word1>')
-def my_color(word1):
-    return '<h1 style="color:Red">' + word1 + '</h1>'
+@app.route('/pop/<abbrev>')
+def my_pop(abbrev):
+    my_abbreviation = abbrev
+    cur.execute("SELECT * FROM statepopulations WHERE code = %s", [my_abbreviation],)
+    row = cur.fetchone()
+    my_population = row[2]
+    the_string = "The population of" + my_abbreviation " is: " + my_population;
+    return the_string
+
 
 if __name__ == '__main__':
     my_port = 5132
