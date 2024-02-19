28e2a848696ea04a8beb5d8314d3819421bf08bf
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2024-02-19 09:06:25 -0600

    add button to show time from friday

--- csslab/static/example.js
+++ csslab/static/example.js
@@ -2,3 +2,11 @@ function changeColor() {
   the_heading = document.getElementById("hello");
   the_heading.style.color = "red";
 }
+
+function displayTime() {
+	const my_date = new Date();
+  let the_hour = my_date.getHours().toString();
+  let the_minutes = my_date.getMinutes().toString();
+	time.innerHTML = the_hour + ":" + the_minutes;
+}
+
