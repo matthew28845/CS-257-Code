64d72944aca8946cae4a17f1c0db38a9f90608c6
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2024-02-19 09:08:51 -0600

    make time look better

--- csslab/static/example.js
+++ csslab/static/example.js
@@ -7,6 +7,9 @@ function displayTime() {
 	const my_date = new Date();
   let the_hour = my_date.getHours().toString();
   let the_minutes = my_date.getMinutes().toString();
+  if (the_minutes.length == 1) {
+      the_minutes = "0" + the_minutes;
+  }
 	time.innerHTML = the_hour + ":" + the_minutes;
 }
 
